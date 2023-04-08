fb11b055cca218da4fa0bfbabcc9e8e3d4649de1
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-04-08 18:29:15 +0200

    descargo iamegenes de una web

--- asincrono.py
+++ asincrono.py
@@ -36,3 +36,61 @@ async def get_uri_from_images_src(base_uri, images_src):
         else:    
             yield parsed.geturl()
             await asyncio.sleep(0.001) 
+            
+async def get_images(session, page_uri):  
+    html = await wget(session, page_uri)  
+    if not html:  
+        print("Error: no se ha encontrado ninguna imagen", 
+        sys.stderr)  
+        return None  
+    images_src_gen = get_images_src_from_html(html)  
+    images_uri_gen = get_uri_from_images_src(page_uri, 
+        images_src_gen)  
+    async for image_uri in images_uri_gen:  
+        print('Descarga de %s' % image_uri)  
+        await download(session, image_uri) 
+
+
+async def wget(session, uri):    
+    """Descarga una URI y devuelve el contenido"""    
+    async with session.get(uri) as response:    
+        if response.status == 200:
+            if response.content_type == 'text/html':   
+                return await response.text() 
+            else:    
+                return await response.read()   
+        else:    
+            return None
+        
+async def download(session, uri):    
+    """Descarga una URI y la guarda en el disco"""    
+    async with session.get(uri) as response:    
+        if response.status == 200:    
+            if response.content_type == 'text/html':    
+                return None    
+            else:    
+                filename = uri.split('/')[-1]    
+                with open(filename, 'wb') as f:    
+                    while True:    
+                        chunk = await response.content.read(1024)    
+                        if not chunk:    
+                            break    
+                        f.write(chunk)    
+                return filename    
+        else:    
+            return None
+        
+async def main():
+    async with aiohttp.ClientSession() as session:    
+        await get_images(session, 'https://viajes.nationalgeographic.com.es/a/mejores-playas-espana_11759')
+    
+    
+def write_in_file(filename, content):
+    with open(filename, 'w') as f:
+        f.write(content)
+    
+        
+if __name__ == '__main__':
+    time = timeit(main, number=1)
+    write_in_file('time.txt', str(time))
+    asyncio.run(main())        
